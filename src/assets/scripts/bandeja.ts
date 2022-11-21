19c6777e756958d3212300d4e4506d47cd8c427b
Yuri Silva <yurikgs@outlook.com>
2022-11-21 10:34:40 -0300

    FEAT: tipos adicionados à pasta types, e funcionando corretamente

--- src/assets/scripts/bandeja.ts
+++ src/assets/scripts/bandeja.ts
@@ -1,3 +1,5 @@
+import { Hamburguer } from "./types/Hamburguer";
+import { Ingredient } from "./types/Ingredient";
 
 const page = document.querySelector("body[data-display='bandeja']")
 
@@ -5,34 +7,26 @@ if(page) {
     
     // Mock de listagem de ingredientes (deve ser puxado do firebase)
 
-        // types
-        type tiposPaesItem = {
-            name: string;
-            price: string;
-        }
-        type tiposIngredientesItem = {
-            name: string;
-            price: string;
-        }
-
     const opcoesEl = page.querySelectorAll(".category")
     const tiposPaesEl = opcoesEl[0] as HTMLElement
     const tiposIngredientesEl = opcoesEl[1] as HTMLElement
 
-    const tiposPaes: tiposPaesItem[] = []
-    const tiposIngredientes: tiposIngredientesItem[] = []
+    const tiposPaes: Ingredient[] = []
+    const tiposIngredientes: Ingredient[] = []
 
     // tiposPaes
     const tiposPaesh3El = tiposPaesEl.querySelectorAll("h3")
     const tiposPaesPriceEl = tiposPaesEl.querySelectorAll("div")
     for(let i = 0; i < tiposPaesh3El.length; i++) {
         
-        let item: tiposPaesItem = {
+        let item: Ingredient = {
             name: "",
-            price: ""
+            price: 0
         }
         item.name = tiposPaesh3El[i].innerText
-        item.price = tiposPaesPriceEl[i].innerText
+        item.price = parseFloat(tiposPaesPriceEl[i].innerText.split(" ")[1].replace(",", "."))
+
+        // console.log("item.price", item.price, tiposPaesPriceEl[i].innerText.split(" ")[1])
 
         tiposPaes.push(item)
 
@@ -45,12 +39,14 @@ if(page) {
     const tiposIngredientesPriceEl = tiposIngredientesEl.querySelectorAll("div")
     for(let i = 0; i < tiposIngredientesh3El.length; i++) {
         
-        let item: tiposIngredientesItem = {
+        let item: Ingredient = {
             name: "",
-            price: ""
+            price: 0
         }
         item.name = tiposIngredientesh3El[i].innerText
-        item.price = tiposIngredientesPriceEl[i].innerText
+        item.price = Number(tiposIngredientesPriceEl[i].innerText.split(" ")[1].replace(",", "."))
+
+        // console.log("item.price", item.price)
 
         tiposIngredientes.push(item)
 
@@ -58,19 +54,11 @@ if(page) {
     console.log(tiposIngredientes)
 
 
-// types
-
-type hamburguer = {
-    name: string;
-    price: string;
-}
-
-
 // Carregar os hamburgueres selecionados no resumo
 
 const bandejaEl = page.querySelector("aside ul") as HTMLUListElement
 const bandejaTitulo = page.querySelector("header small") as HTMLElement
-let hamburgueres: hamburguer[] = []
+let hamburgueres: Hamburguer[] = []
 
 const limpaBandeja = () => {
     bandejaEl.innerHTML = ""
@@ -87,13 +75,13 @@ const atualizaHamburgueres = () => {
         
         
         let hamburguer = {
-            name: hamburguerElData[0].innerHTML,
-            price: hamburguerElData[1].innerHTML
+            description: hamburguerElData[0].innerHTML,
+            price: parseFloat(hamburguerElData[1].innerHTML.split(" ")[1].replace(",", "."))
         }
         
         hamburgueres.push(hamburguer)
 
-        // console.log("Hamburgueres: ", hamburgueres)
+        
     });
 }
 
---
+++ src/assets/scripts/types/Ingredient.ts
@@ -0,0 +1,5 @@
+export type Ingredient = {
+    id?: number;
+    name: string;
+    price: number;
+}
