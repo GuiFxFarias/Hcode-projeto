b20c5741526f6c2847c9931cbe3475c37e5774b6
GuiFxFarias <109701085+GuiFxFarias@users.noreply.github.com>
2022-11-23 19:49:24 -0300

    criação do firebase

---
+++ public/assets/scripts/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC05geY2ZWQOSDne-J0_tFRtIH5FcTo6HU",
+  authDomain: "hburger-projeto.firebaseapp.com",
+  projectId: "hburger-projeto",
+  storageBucket: "hburger-projeto.appspot.com",
+  messagingSenderId: "131149991908",
+  appId: "1:131149991908:web:37831fdc2e36c853f4c1f1"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
